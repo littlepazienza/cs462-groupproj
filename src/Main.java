97e791cf9a61aae6f997917241164634d01c4101
bpazienza <ben.pazienza@alertinnovation.com>
2021-04-07 17:59:14 -0400

    Fixing the files and implementing the algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * This class executes the LCS algorithm against two files given by arguments to the JVM
+ *
+ * @author Jeffrey Umanzor
+ * @author Benjamin Pazienza
+ * @version 1.0
+ */
 public class Main{
 
   public static void main(String[] args) {
@@ -25,10 +32,43 @@ public class Main{
     }
   }
 
+  /**
+   * This function compares two string arrays to get the longest common subsequence between
+   * the two and prints it out with pretty formating
+   *
+   * @param file1 an array of Strings for each line in the first file that was read
+   * @param file2 an array of Strings for each line in the second file that was read
+   */
   private static void compareFiles(String[] file1, String[] file2) {
-    System.out.printf("Feedback for 1st file\n[%s] is:\n", Arrays.toString(file1));
-    System.out.printf("Feedback for 2nd file\n[%s] is:\n", Arrays.toString(file2));
-    // TODO: 4/7/21 LMAO FIGURE OUT HOW TO ACTUALLY DO THIS
+
+    ArrayList<String> lcs = new ArrayList<>();
+    int lcs_table[][] = new int[file1.length + 1][file2.length + 1];
+
+    for (int i = 0; i <= file1.length; i++) {
+      for (int j = 0; j <= file2.length; j++) {
+        if (i == 0 || j == 0) {
+          lcs_table[i][j] = 0;
+        } else if (file1[i - 1].equals(file2[j - 1])) {
+          lcs_table[i][j] = lcs_table[i - 1][j - 1] + 1;
+          lcs.add(file1[i-1]);
+        } else {
+          lcs_table[i][j] = Math.max(lcs_table[i - 1][j], lcs_table[i][j - 1]);
+        }
+      }
+    }
+
+    System.out.printf("# of common lines: %s\n", lcs_table[file1.length][file2.length]);
+    System.out.printf("The common lines are\n%s\n\n", lcs);
+    System.out.printf("Feedback for 1st file:\n%s is:\n", Arrays.toString(file1));
+    Arrays.stream(file1).forEach((s) -> {
+      System.out.println(lcs.contains(s) ? s + " is in both files": s + " is only in this file");
+    });
+    System.out.println();
+    System.out.printf("Feedback for 2nd file\n%s is:\n", Arrays.toString(file2));
+    Arrays.stream(file2).forEach((s) -> {
+      System.out.println(lcs.contains(s) ? s + " is in both files": s + " is only in this file");
+    });
+
   }
 
 }
